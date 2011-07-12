e5769dcb3b28f41006de0ed35ac24dfe0ddf0b48
Maximilian Marcoll <quince@maximilianmarcoll.de>
2011-07-12 11:51:08 +0200

    added function: EqlDstrbtn_Pitch
    Signed-off-by: Maximilian Marcoll <quince@maximilianmarcoll.de>

---
+++ bundles/Functions/EqlDstrbtn_Pitch/EqlDstrbtn_Pitch.h
@@ -0,0 +1,36 @@
+//
+//  EqlDstrbtn_Pitch.h
+//  quince
+//
+//  Created by max on 7/12/11.
+//  Copyright 2011 Maximilian Marcoll. All rights reserved.
+//
+//
+//	If you have any questions contact quince@maximilianmarcoll.de
+//
+//	This file is part of quince.
+//
+//	quince is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	quince is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with quince.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <QuinceApi/Function.h>
+#import "QuinceObject.h"
+
+@interface EqlDstrbtn_Pitch : Function {
+@private
+    
+}
+
+@end
---
+++ bundles/Functions/EqlDstrbtn_Pitch/EqlDstrbtn_Pitch.m
@@ -0,0 +1,59 @@
+//
+//  EqlDstrbtn_Pitch.m
+//  quince
+//
+//  Created by max on 7/12/11.
+//  Copyright 2011 Maximilian Marcoll. All rights reserved.
+//
+//
+//	If you have any questions contact quince@maximilianmarcoll.de
+//
+//	This file is part of quince.
+//
+//	quince is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	quince is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with quince.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "EqlDstrbtn_Pitch.h"
+
+
+@implementation EqlDstrbtn_Pitch
+
+
+-(void)perform{
+	
+	QuinceObject * quince, * mother = [self objectForPurpose:@"source"];
+	[mother sortByKey:@"frequency" ascending:YES];
+	
+	double max  = -1000, incrementCent, min = [[[[mother valueForKey:@"subObjects"]objectAtIndex:0]valueForKey:@"frequency"]doubleValue], f , factor;
+	int i, count = [mother subObjectsCount];
+	for(quince in [mother valueForKey:@"subObjects"]){
+		if([[quince valueForKey:@"frequency"]doubleValue]>max)
+			max = [[quince valueForKey:@"frequency"]doubleValue];
+	}
+	
+	incrementCent = 1200 * log2(max/min) / (count-1);
+	
+	for(i=0;i<count;i++){
+		quince = [[mother valueForKey:@"subObjects"]objectAtIndex:i];
+        factor = pow(pow(pow(2, 1.0/1200.0), incrementCent), i+1);
+        f = min * factor;
+		[quince setValue:[NSNumber numberWithDouble:f] forKey:@"frequency"];
+	}
+	
+    [mother update];
+	[self setOutputObjectToObjectWithPurpose:@"source"];
+	[self done];
+}
+
+@end
