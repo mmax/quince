a7d4bc99a46bcc0f4cddaef48f2c86f82faa9714
Maximilian Marcoll <quince@maximilianmarcoll.de>
2013-03-10 18:11:19 -0600

    added ReverseLegato Function
    Signed-off-by: Maximilian Marcoll <quince@maximilianmarcoll.de>

---
+++ bundles/Functions/ReverseLegato/ReverseLegato.m
@@ -0,0 +1,55 @@
+//
+//  ReverseLegato.m
+//  quince
+//
+//  Created by Maximilian Marcoll on 3/10/13.
+//  Copyright (c) 2013 Maximilian Marcoll. All rights reserved.
+//
+//
+//	If you have any questions contact quince@maximilianmarcoll.de
+//
+//	This file is part of quince.
+//
+//	quince is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	quince is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with quince.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "ReverseLegato.h"
+
+@implementation ReverseLegato
+
+
+-(void)perform{
+	
+	QuinceObject * quince, * mother = [self objectForPurpose:@"source"];
+	NSArray * subs = [mother valueForKey:@"subObjects"];
+	double endA, endB;	
+	[mother sortChronologically];
+	
+	for(int i=1;i<[subs count];i++){
+		quince = [subs objectAtIndex:i];
+		endA = [[[subs objectAtIndex:i-1]end]doubleValue];// [[quince valueForKey:@"start"]doubleValue];
+        endB = [[quince end]doubleValue];
+		[quince setValue:[NSNumber numberWithDouble:endB-endA] forKey:@"duration"];
+        [quince setValue: [NSNumber numberWithDouble:endA] forKey:@"start"];
+	}
+    
+	[self setOutputObjectToObjectWithPurpose:@"source"];
+	[self done];
+}
+
+
+
+
+
+@end
