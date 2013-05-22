40e28ac37a5a18d52a84eb95f190ca69fe330194
Maximilian Marcoll <quince@maximilianmarcoll.de>
2013-05-22 23:43:24 +0200

    RemoveGlissUp
    Signed-off-by: Maximilian Marcoll <quince@maximilianmarcoll.de>

---
+++ bundles/Functions/RemoveGlissUp/RemoveGlissUp.m
@@ -0,0 +1,56 @@
+//
+//  RemoveGlissUp.m
+//  quince
+//
+//  Created by Maximilian Marcoll on 5/22/13.
+//  Copyright (c) 2013 Maximilian Marcoll. All rights reserved.
+//
+//
+//	If you have any questions contact quince@maximilianmarcoll.de
+//
+//	This file is part of quince.
+//
+//	quince is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	quince is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with quince.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#import "RemoveGlissUp.h"
+
+@implementation RemoveGlissUp
+
+
+-(void)perform{
+	
+	QuinceObject * q, * mother = [self objectForPurpose:@"source"];
+    QuinceObject * result = [self outputObjectOfType:@"QuinceObject"];
+    
+	[mother sortChronologically];
+    
+    for(QuinceObject * sub in [mother valueForKey:@"subObjects"]){
+        if([[sub valueForKey:@"glissandoDirection"]intValue] == 0 || ![sub valueForKey:@"glissandoDirection"]){
+            q = [sub copyWithZone:nil];
+            [[result controller] addSubObjectWithController:[q controller] withUpdate:NO];
+        }
+    }
+    
+    [[result controller] update];
+	[[result controller] setValue:[NSString stringWithFormat:@"%@_RmvGlssUp", [mother valueForKey:@"name"]] forKeyPath:@"selection.name"];  
+	
+    
+	[self done];
+}
+
+
+@end
+
