72fd1f644a25d481a7987113eb28c05b7d83f9e8
Maximilian Marcoll <quince@maximilianmarcoll.de>
2016-01-09 13:10:51 +0100

    ExportSubObjects numbering update (still not bug free (one additional file is being created))
    Signed-off-by: Maximilian Marcoll <quince@maximilianmarcoll.de>

--- bundles/Functions/ExportSubObjects/ExportSubObjects.m
+++ bundles/Functions/ExportSubObjects/ExportSubObjects.m
@@ -13,24 +13,29 @@
 -(void)perform{
     
     QuinceObject * mum = [self objectForPurpose:@"source"];
-    NSString *t;
+    NSString *t, *n;
     NSOpenPanel * sp = [NSOpenPanel openPanel];
     [sp setCanChooseDirectories:YES];
     [sp setCanCreateDirectories:YES];
 	[sp setTitle:@"Save SubObjects"];
-    int i=0, status = [sp runModal];
+    int status = [sp runModal];
+    long i=1;
     
 	if(status==NSFileHandlingPanelOKButton){
         
         for(QuinceObject * q in [mum valueForKey:@"subObjects"]){
-            
-            t = [NSString stringWithFormat:@"%@/%@_%d.xml", [[sp URL]path], [mum valueForKey:@"name"], ++i]; 
+            if([[q valueForKey:@"name"]isEqualToString:@"untitled"])
+                n = [NSString stringWithFormat:@"%ld_%@.xml", i, [mum valueForKey:@"name"]];
+            else
+                n = [NSString stringWithFormat:@"%ld_%@.xml", i, [q valueForKey:@"name"]];
+            t = [NSString stringWithFormat:@"%@/%@", [[sp URL]path], n]; 
             
             if(![[q xmlDictionary] writeToFile:t  atomically:YES]){
                 
                 [document presentAlertWithText:[NSString stringWithFormat:@"ExportSubObjects: could not write file: %@", t]];
                 break;
             }
+            i++;
         }
 	}
     
